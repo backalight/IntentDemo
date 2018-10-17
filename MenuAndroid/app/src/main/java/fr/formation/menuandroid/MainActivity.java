733231b8111d9abd279eceb1f8378edc6fd9a720
backalight <antoine.soros@orange.fr>
2018-10-17 14:50:07 +0200

    menu android

---
+++ MenuAndroid/app/src/main/java/fr/formation/menuandroid/MainActivity.java
@@ -0,0 +1,80 @@
+package fr.formation.menuandroid;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_parametre) {
+            return true;
+        }
+        if (id == R.id.actionActivit0) {
+            return true;
+        }
+        if (id == R.id.actionActivit1) {
+            return true;
+        }
+        if (id == R.id.actionActivit2) {
+            return true;
+        }
+        if (id == R.id.actionActivit3) {
+            return true;
+        }
+        if (id == R.id.connexion) {
+            if (item.isChecked())
+            item.setChecked(false);
+
+            else {
+                item.setChecked(true);
+            }
+            return true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
+
